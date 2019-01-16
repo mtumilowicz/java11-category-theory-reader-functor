a4ca67f00c5782c9b27160916fbbc0816a153475
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-16 17:59:00 +0100

    map should return Reader (not Function) and fix in tests

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -5,7 +5,11 @@ import java.util.function.Function;
  */
 @FunctionalInterface
 interface Reader<R, A> extends Function<R, A> {
-    default <B> Function<R, B> map(Function<A, B> f) {
-        return f.compose(this);
+    default <B> Reader<R, B> map(Function<A, B> f) {
+        return asReader(f.compose(this));
+    }
+    
+    default <X, Y> Reader<X, Y> asReader(Function<X, Y> f) {
+        return f::apply;
     }
 }
--- src/test/java/ReaderTest.java
+++ src/test/java/ReaderTest.java
@@ -1,10 +1,9 @@
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.function.Function;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by mtumilowicz on 2019-01-02.
@@ -14,7 +13,7 @@ public class ReaderTest {
     @Test
     public void map() {
         Reader<BigDecimal, Integer> toInteger = BigDecimal::intValue;
-        Function<BigDecimal, String> mapped = toInteger.map(String::valueOf);
+        Reader<BigDecimal, String> mapped = toInteger.map(String::valueOf);
         
         assertThat(mapped.apply(BigDecimal.TEN), is("10"));
     }
